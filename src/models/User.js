95369ede09a5da3ca3164a4822fdad31e9b25959
Venom-bot460 <sharrellaure@gmail.com>
2026-08-18 05:19:19 +0100

    work on user models

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,11 @@ import mongoose from 'mongoose';
  */
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     email: {
       type: String,
       unique: true,
@@ -17,7 +22,17 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      default: 'user',
+      enum: ['student', 'tutor'],
+      default: 'student',
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    subjectTags: {
+      type: [String],
+      default: [],
     },
   },
   { timestamps: true }
