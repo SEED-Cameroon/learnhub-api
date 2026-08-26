fde90d2963dd8c9f6798fc0e5008b4bb03938316
Venom-bot460 <sharrellaure@gmail.com>
2026-08-26 16:38:15 +0100

    Modification of tutor middleware

---
+++ src/middleware/role.js
@@ -0,0 +1,14 @@
+export function requireRole(...allowedRoles) {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden",
+      });
+    }
+
+    next();
+  };
+}
+
+export default requireRole;
